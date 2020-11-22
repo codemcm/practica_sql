c86b1cfe5c2f9fc038e2be9f7b5dec6a7d11ab25
martin <codemcm@hotmail.com>
2020-11-22 12:30:16 -0600

    queries user add

---
+++ event_uc/queriesMartin.sql
@@ -0,0 +1,19 @@
+INSERT INTO user_uc (user_name, password, created_by) 
+    VALUES ('MARTIN_UC', '456', 1);
+
+INSERT INTO user_uc (user_name, password, created_by) 
+    VALUES ('yael_uc', '789', 1);
+
+INSERT INTO user_uc (user_name, password, created_by) 
+    VALUES ('Ricardo_uc', '789', 1);
+
+INSERT INTO user_uc (user_name, password, created_by) 
+    VALUES ('Liz_uc', '789', 1);
+
+
+INSERT INTO user_uc (user_name, password, created_by) 
+    VALUES ('Liz_uc', '789', 1);
+
+
+INSERT INTO event_uc (name, organizer_id, created_by, edited_by)
+VALUES('Taller rest api Node', 1, 1,1);
